b0d2fbfa9c54a3605c0147fe63a834f8d03564fa
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2022-07-29 13:05:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const Flowers Shop = document.querySelector(".flowers");
-const navMenu = document.querySelector(".nav-menu");
-
-flowers.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    flowers.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
